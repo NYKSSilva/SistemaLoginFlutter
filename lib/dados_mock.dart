8c6b05a120032ca884c011044a9094aca6a24801
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-08-11 11:25:37 -0300

    Atualizando a pagina de login

---
+++ lib/dados_mock.dart
@@ -0,0 +1,7 @@
+List<Map<String, String>> usuarios =[
+  {
+    'nome': 'Administrador',
+    'email': 'adm@gmail.com',
+    'senha': '1234'
+  },
+];
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../dados_mock.dart';
 
 class LoginPage extends StatefulWidget{
   const LoginPage({super.key});
@@ -9,6 +10,39 @@ class LoginPage extends StatefulWidget{
 
 class _LoginPageState extends State<LoginPage>{
 
+final TextEditingController emailController = TextEditingController();
+final TextEditingController senhaController = TextEditingController();
+
+bool esconderSenha = true;
+
+void mostrarMensagem(String mensagem){
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(mensagem)
+  ),
+  );
+}
+
+void entrar(){
+  String email = emailController.text.trim();
+  String senha = senhaController.text;
+
+  if(email.isEmpty || senha.isEmpty){
+    mostrarMensagem('Preencha o email e a senha.',);
+    return;
+  }
+  Map<String, String>? usuarioEncontrado;
+
+  for (var usuario in usuarios){
+    if(
+      usuario['email'] == email &&
+      usuario['senha'] == senha
+    ){
+      mostrarMensagem('Login efetuado com sucesso',);
+      // usuarioEncontrado = usuario;
+      break;
+    }
+  }
+}
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -16,9 +50,91 @@ class _LoginPageState extends State<LoginPage>{
         title: const Text('Login'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Tela de Login'),
-      ),
+      body: SingleChildScrollView(
+        padding:  const EdgeInsets.all(24),
+        child: Column(
+          children: [
+             const SizedBox(height: 40,),
+
+            const Icon(
+              Icons.account_circle,
+              size: 100,
+              ),
+
+              const SizedBox(height: 20 ,),
+
+              const Text(
+                'Bem-vindo',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+
+              const SizedBox(height: 5,),
+
+              const Text(
+                'Entre com sua conta para acessar o sistema',
+                textAlign: TextAlign.center,
+              ),
+
+               const SizedBox(height: 30 ,),
+
+               TextField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "E-mail",
+                  hintText: "Digite seu email",
+                  prefixIcon: Icon(Icons.email),
+                  border:OutlineInputBorder()
+                )
+               ),
+
+
+              const SizedBox(height: 15 ,),
+
+               TextField(
+                controller: senhaController,
+                obscureText: esconderSenha,
+                keyboardType: TextInputType.emailAddress,
+                decoration:  InputDecoration(
+                  labelText: "Senha",
+                  hintText: "Digite sua seha",
+                  prefixIcon: const Icon(Icons.lock),
+                  border: const OutlineInputBorder(),
+
+                  suffixIcon: IconButton(
+                    onPressed: (){
+                      setState(() {
+                        esconderSenha = !esconderSenha;
+                      });
+                    }, icon: Icon(
+                        esconderSenha? Icons.visibility : Icons.visibility_off
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 25,),
+
+            ElevatedButton.icon(onPressed: entrar,
+            icon: Icon(Icons.login),
+            label: const Text('Entrar'),
+            ),
+
+            const SizedBox(height: 10,),
+
+            OutlinedButton.icon(
+              onPressed: (){}, 
+              icon: Icon(Icons.person_add),
+              label: const Text('Criar usuário'),
+              )
+
+          ],
+        ),
+      )
     );
   }
 }
